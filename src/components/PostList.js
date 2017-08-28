9a6ef504840840639c899aeb82b8b4ed220e154e
hsjung22 <hsjung22@gmail.com>
2017-08-27 18:26:53 -0700

    feat: put post as a list

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -5,6 +5,7 @@ import moment from 'moment'
 function PostList (props) {
   return (
     <div>
+      <h3>Posts ({props.posts.length})</h3>
       <button onClick={() => {props.toggleSort({ posts: "-voteScore" })}}>
         Vote Score
       </button>
@@ -12,17 +13,22 @@ function PostList (props) {
       <button onClick={() => {props.toggleSort({ posts: "-timestamp" })}}>
         Date
       </button>
-      <br />
 
-      {props.posts.map(post =>
-        <div key={post.id}>
-          <Link to={`/${post.category}/${post.id}`}>
-            {post.title}
-          </Link>
-          Vote Score: {post.voteScore}
-          Date: {moment(post.timestamp).format("l")}
-        </div>
-      )}
+      <hr />
+
+
+      <ul className="list-group">
+        {props.posts.map(post =>
+          <li key={post.id} className="list-group-item">
+            <Link to={`/${post.category}/${post.id}`}>
+              {post.title}
+            </Link>
+            Vote Score: {post.voteScore}
+            Date: {moment(post.timestamp).format("l")}
+          </li>
+        )}
+      </ul>
+
     </div>
   )
 }
